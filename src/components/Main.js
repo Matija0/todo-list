d42fdb5a535a967f1e7949177de2f1f0038f1830
Matija0 <90220699+Matija0@users.noreply.github.com>
2023-02-17 21:46:57 +0100

    Main and index commit
    Fixed edit input and added task complete check

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,7 @@ import {useEffect} from 'react';
 import Modal from 'react-modal';
 import {MdDelete} from "react-icons/md"
 import {AiFillEdit} from "react-icons/ai"
+import {AiFillCheckCircle} from "react-icons/ai"
 
 export default function Main(){
     const LOCAL_STORAGE_KEY = "dataKey"
@@ -13,9 +14,11 @@ export default function Main(){
     const [title, setTitle] = useState('');
     const [status, setStatus] = useState(false);
     const [editModal, setEditModal]= useState(false)
-    const [edit_title, setEditTitle]=useState()
+    const [edit_title, setEditTitle]=useState("")
+    const [new_title, setNewTitle]=useState("")
     const [edit_status, setEditstatus]=useState(false)
     const [edit_id, setEditId]=useState()
+    
     const customStyles = {
         content: {
           top: '50%',
@@ -29,7 +32,9 @@ export default function Main(){
       Modal.setAppElement('#root'); 
     const [modalIsOpen, setIsOpen] = React.useState(false);
 
-      
+     const changeEditTitle=(val)=>{
+      setEditTitle(val)
+     } 
   function openModal() {
     setIsOpen(true);
   }
@@ -64,7 +69,7 @@ useEffect(() => {
 const update=(id)=>{
 let updates = data.map((item)=>{
     if(item.id === id){
-        return {id:id,title:edit_title,status:edit_status}
+        return {id:id,title:new_title,status:edit_status}
     }
     return item
 })
@@ -86,21 +91,37 @@ function remove(item){
     array.splice(index, 1)
     setData([...array])
 }
-const cards=data.map((item,index)=>{
-    return(
- 
-    <Card
-    key={index}
-        item={item}
-        delete={()=>remove(item)}/>
- 
-    )}) 
 
+
+
+ const Card=(props)=>{
+  return(
+     
+         
+      <tr>
+        <th scope="row">{props.item.id}</th>
+        <td>{props.item.title}</td>
+        <td className={props.item.status===1? "Active": "Completed"}>{props.item.status===1? "Active": "Completed"}</td>
+        <td><button className="btn btn-outline-success" onClick={()=>open_edit_modal(props.item)}><AiFillEdit/></button></td>
+        <td><button className="btn btn-outline-danger" onClick={props.delete}><MdDelete/></button></td>
+   
+      </tr>
+     
+
+    
+  )
+}
+
+
+const number =data.filter(v => v.status==0)
+const number2= data.filter(v=> v.status==1)
 
     return(
         <div className="main">
-            <button className="btn btn-dark mt-3 mb-3" onClick={openModal}>ADD TASK</button>
-      
+            <div className="nav">
+              <button className="btn btn-dark mt-3 mb-3" onClick={openModal}>ADD TASK</button>
+              <div id="task-complete" className="mt-3"><AiFillCheckCircle />{Object.keys(number).length}/{Object.keys(number2).length}</div>
+            </div>
             <Modal
                 isOpen={modalIsOpen}
                 onRequestClose={closeModal}
@@ -125,7 +146,36 @@ const cards=data.map((item,index)=>{
     </tr>
   </thead>
   <tbody>
-  {cards}
+  {data.map((item,index)=>(
+     <>
+     {editModal && edit_id === item.id?
+       <div style={{position:"absolute",top:"50%",bottom:"auto",right: "auto", left:"50%" ,width:"100%",height:"100%", marginRight: '-50%',
+       transform: 'translate(-50%, -50%)',zIndex:333,background:"#ddd"}}
+       >
+<h1>Edit {edit_title}</h1>
+<form>
+<input className="edit-input" defaultValue={edit_title}  onChange={(e)=>setNewTitle(e.target.value)} />
+<select className="ml-1" onChange={(e)=> setEditstatus(e.target.value)}>
+<option  selected={!edit_status} value={0}>Completed</option>
+<option  selected={edit_status==1}  value={1}>Active</option>
+
+
+</select>
+</form>
+<br/>
+<div className="btn-container">
+<button className="btn btn-success" onClick={()=> update(edit_id)}>Update</button>
+
+<button onClick={()=>setEditModal(false)} className="btn btn-danger">Close</button>
+</div>
+       </div>:null}
+       <Card
+        key={index}
+        item={item}
+        delete={()=>remove(item)}/></>
+   
+      )
+  )}
   </tbody>
 </table>
         </div>
@@ -133,39 +183,5 @@ const cards=data.map((item,index)=>{
     
     
 
-    function Card(props){
-        return(
-           
-               
-            <tr>
-              <th scope="row">{props.item.id}</th>
-              <td>{props.item.title}</td>
-              <td>{props.item.status==1? "Active":"Completed"}</td>
-              <td><button className="btn btn-outline-success" onClick={()=>open_edit_modal(props.item)}><AiFillEdit/></button></td>
-              <td><button className="btn btn-outline-danger" onClick={props.delete}><MdDelete/></button></td>
-              {editModal && edit_id === props.item.id?
-              <div style={{position:"fixed",top:0,bottom:0,right:0,width:"60%",height:"100%",zIndex:333,background:"#ddd"}}
-              >
-<h1>Edit {edit_title}</h1>
 
-<input className="edit-input" defaultValue={edit_title}  onChange={(e)=>setEditTitle(e.target.value)} />
-<select className="ml-1" onChange={(e)=> setEditstatus(e.target.value)}>
-    <option selected={!edit_status} value={0}>Completed</option>
-    <option selected={edit_status==1}  value={1}>Active</option>
-
-    
-</select>
-<br/>
-<div className="btn-container">
-<button className="btn btn-success" onClick={()=> update(edit_id)}>Update</button>
-
-<button onClick={()=>setEditModal(false)} className="btn btn-danger">Close</button>
-</div>
-              </div>:null}
-            </tr>
-           
-      
-          
-        )
-    }
 }
